7ba34b4ac50d005ab5ce4915934928ef26e543d7
bbzzyyczczeek <bbzzyyczczeek@interia.pl>
2023-07-01 16:00:36 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+    private static String palindrom="kajak";
+    public static void main(String[] args) {
+        boolean palindrome = isPalindrome(palindrom);
+        System.out.println(palindrome);
+
+    }
+   private static boolean isPalindrome(String word){
+       StringBuilder str=new StringBuilder();
+        for (int i = word.length()-1; i >=0 ; i--) {
+            str.append(word.charAt(i));
+        }
+       return word.equals(str.toString());
+   }
+
+}
